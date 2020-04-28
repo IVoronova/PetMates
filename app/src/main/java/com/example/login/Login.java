512220cbe43d5fedf3bdf3f5db69a931dbe6743a
Rayzhu5100 <rayzhu5100@gmail.com>
2020-04-27 21:59:06 -0700

    Tested, register,login and database worked, fixed little bug

--- app/src/main/java/com/example/login/Login.java
+++ app/src/main/java/com/example/login/Login.java
@@ -39,24 +39,16 @@ public class Login extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),"Successfully Login",Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(Login.this, MainActivity.class);
                     startActivity(intent);
-                }else{
-                    Toast.makeText(getApplicationContext(),"Worng email or password",Toast.LENGTH_SHORT).show();
-                    validate(emailValue, passwordValue);
+                }else {
+                    counter--;
+                    Toast.makeText(getApplicationContext(), "Worng email or password!\nNumber of attempts remaining:" + String.valueOf(counter), Toast.LENGTH_SHORT).show();
+                    if (counter == 0) {
+                        login.setEnabled(false); //disable log button after three times
+                    }
                 }
-
-
             }
         });
-
     }
+}
 
-    private void validate(String userName, String userPassword){
-
-            counter--;
-            Toast.makeText(getApplicationContext(),"Number of attempts remaining:" + String.valueOf(counter),Toast.LENGTH_SHORT).show();
-            if(counter == 0){
-                login.setEnabled(false); //disable log button after three times
-            }
-        }
-    }
 
