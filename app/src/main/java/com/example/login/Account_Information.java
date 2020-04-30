22baf0c88582e0056477d758252de423bb8fa658
AnnaCui2 <44214199+AnnaCui2@users.noreply.github.com>
2020-04-29 19:20:29 -0700

    added account information and preferences

---
+++ app/src/main/java/com/example/login/Account_Information.java
@@ -0,0 +1,37 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Account_Information extends AppCompatActivity {
+    EditText Email, Password, Comfirm_Password, Phone_Number;
+    Button Save, Back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account__information);
+
+        Email = (EditText)findViewById(R.id.etAccount_email);
+        Password = (EditText)findViewById(R.id.etAccount_Password);
+        Comfirm_Password = (EditText)findViewById(R.id.etAccount_comfirm);
+        Phone_Number = (EditText)findViewById(R.id.etAccount_phone);
+
+        Save = (Button)findViewById(R.id.btnAccount_save);
+        Back = (Button)findViewById(R.id.btnAccount_back);
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Account_Information.this, Profile.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
