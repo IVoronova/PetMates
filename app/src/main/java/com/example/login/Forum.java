25a638fb444326986c0ce12d46a92a8c02983757
AnnaCui2 <44214199+AnnaCui2@users.noreply.github.com>
2020-05-03 19:48:35 -0700

    Create Pet news

--- app/src/main/java/com/example/login/Forum.java
+++ app/src/main/java/com/example/login/Forum.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Forum extends AppCompatActivity {
 
     TextView back;
+    Button Pet_News, Gallery, Lost_and_Found, Pet_Jobs, Report;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +21,22 @@ public class Forum extends AppCompatActivity {
         Intent intent = getIntent();
         final String email = intent.getStringExtra("email");
 
-        back = findViewById(R.id.btnBackF);
+        back = (TextView) findViewById(R.id.btnBackF);
+        Pet_News = (Button) findViewById(R.id.btnPet_News);
+        Gallery = (Button) findViewById(R.id.btnGalleries);
+        Lost_and_Found = (Button) findViewById(R.id.btnLost_Found);
+        Pet_News = (Button) findViewById(R.id.btnPet_Jobs);
+        Report = (Button) findViewById(R.id.btnReport);
 
+        Pet_News.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Forum.this, Pet_News.class);
+                startActivity(intent);
+            }
+        });
+
+        //back button
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
