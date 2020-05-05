0e602a69ba3c34100ea38457da3ef791f722ec79
Rayzhu5100 <rayzhu5100@gmail.com>
2020-05-05 16:16:18 -0700

    new database sample

--- app/src/main/java/com/example/login/Friends.java
+++ app/src/main/java/com/example/login/Friends.java
@@ -3,16 +3,20 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Friends extends AppCompatActivity {
     TextView back;
     ListView listView;
-    String[] friendlist;
-    String[] friendimage;
+    DatabaseHelper db;
+    String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,14 @@ public class Friends extends AppCompatActivity {
         setContentView(R.layout.activity_friends);
 
         Intent intent = getIntent();
-        final String email = intent.getStringExtra("email");
+        email = intent.getStringExtra("email");
+
+        List<String> friendlist = new ArrayList<>();
+        Cursor friend_name = db.get_Friendlist(email);
+        while(friend_name.moveToNext()){
+
+
+        }
 
         back = findViewById(R.id.btnBackF);
         back.setOnClickListener(new View.OnClickListener() {
