22602ba33506368564b4346a575183697e7d442e
AnnaCui2 <44214199+AnnaCui2@users.noreply.github.com>
2020-05-04 22:07:04 -0700

    Fixed forum ids

---
+++ app/src/main/java/com/example/login/Lost_and_Found.java
@@ -0,0 +1,14 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lost_and_Found extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_and__found);
+    }
+}
