24b93c2250f51344bc4b77f9ca6e4340ed44e62e
Rayzhu5100 <rayzhu5100@gmail.com>
2020-05-09 18:44:14 -0700

    admin view report

--- app/src/main/java/com/example/login/Pet_News.java
+++ app/src/main/java/com/example/login/Pet_News.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Pet_News extends AppCompatActivity {
     TextView Back, Next, New, Article1, Article2, Article3, Article4;
     String BackRead = "Pet_News",email ;
+    ImageView Pic1, Pic2, Pic3, Pic4;
     DatabaseHelper db;
+    Bitmap bt = null;
     int art1num , art2num, art3num, art4num;
     Cursor cursor;
 
@@ -32,6 +37,10 @@ public class Pet_News extends AppCompatActivity {
         Article2 = findViewById(R.id.tvPetNews_Article2);
         Article3 = findViewById(R.id.tvPetNews_Article3);
         Article4 = findViewById(R.id.tvPetNews_Article4);
+        Pic1 = findViewById(R.id.imageView10);
+        Pic2 = findViewById(R.id.imageView7);
+        Pic3 = findViewById(R.id.imageView9);
+        Pic4 = findViewById(R.id.imageView11);
 
         cursor = db.all_data(1);
         update();
@@ -68,20 +77,32 @@ public class Pet_News extends AppCompatActivity {
 
     public void update(){
         if (cursor.moveToNext()) {
+            byte[] Image = cursor.getBlob(4);
+            bt = BitmapFactory.decodeByteArray(Image,0,Image.length);
             Article1.setText(cursor.getString(2));
             art1num = cursor.getInt(0);
+            Pic1.setImageBitmap(bt);
         }
         if (cursor.moveToNext()) {
+            byte[] Image = cursor.getBlob(4);
+            bt = BitmapFactory.decodeByteArray(Image,0,Image.length);
             Article2.setText(cursor.getString(2));
             art2num = cursor.getInt(0);
+            Pic2.setImageBitmap(bt);
         }
         if (cursor.moveToNext()) {
+            byte[] Image = cursor.getBlob(4);
+            bt = BitmapFactory.decodeByteArray(Image,0,Image.length);
             Article3.setText(cursor.getString(2));
             art3num = cursor.getInt(0);
+            Pic3.setImageBitmap(bt);
         }
         if (cursor.moveToNext()) {
+            byte[] Image = cursor.getBlob(4);
+            bt = BitmapFactory.decodeByteArray(Image,0,Image.length);
             Article4.setText(cursor.getString(2));
             art4num = cursor.getInt(0);
+            Pic4.setImageBitmap(bt);
         }
     }
 
