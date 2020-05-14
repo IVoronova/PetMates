29b666c3b5e1d13ab4b47bee9ab7e9e642dc78cf
Rayzhu5100 <rayzhu5100@gmail.com>
2020-05-13 23:36:10 -0700

    fixed bug

--- app/src/main/java/com/example/login/Register.java
+++ app/src/main/java/com/example/login/Register.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +19,8 @@ public class Register extends AppCompatActivity {
     EditText _Email,_Password,_Repassword,_Phone;
     TextView Back;
     DatabaseHelper db;
+    CheckBox checkbox;
+    boolean check;
 
 
     @Override
@@ -33,43 +36,55 @@ public class Register extends AppCompatActivity {
         _Phone = (EditText) findViewById(R.id.etPhone);
         _Register = (Button) findViewById(R.id.btnRegister);
         Back = findViewById(R.id.btnBackR);
-        //once user check register, check input
-        _Register.setOnClickListener(new View.OnClickListener() {
+        checkbox = findViewById(R.id.checkBox);
+
+        checkbox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String EmailValue = _Email.getText().toString();
-                String PasswordValue = _Password.getText().toString();
-                String RepasswordValue = _Repassword.getText().toString();
-                String PhoneValue = _Phone.getText().toString();
+                check = ((CheckBox) v).isChecked();
+            }
+        });
+            _Register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String EmailValue = _Email.getText().toString();
+                    String PasswordValue = _Password.getText().toString();
+                    String RepasswordValue = _Repassword.getText().toString();
+                    String PhoneValue = _Phone.getText().toString();
 
-                //if one of inputs are empty, send alert
-                if (EmailValue.equals("") || PasswordValue.equals("") || RepasswordValue.equals("") || PhoneValue.equals("")) {
-                    Toast.makeText(getApplicationContext(), "Fields are empty, check your input.", Toast.LENGTH_SHORT).show();
-                } else {//check the email is unique
-                    if (PasswordValue.equals(RepasswordValue)) {
-                        Boolean chkemail = db.email_Unique(EmailValue);
-                        if (chkemail) {//if email is unique, save data to database
-                            boolean insert = db.insert_user(EmailValue, PasswordValue, PhoneValue);
-                            if (insert) {//if successfully insert to database, message and jump to login
-                                Toast.makeText(getApplicationContext(), "Registered Successfully!", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(Register.this, Login.class);
-                                intent.putExtra("email",EmailValue);
-                                startActivity(intent);
-                            }
-                        } else Toast.makeText(getApplicationContext(), "Email already exists", Toast.LENGTH_SHORT).show();
-                    }else Toast.makeText(getApplicationContext(), "Password doesn't match", Toast.LENGTH_SHORT).show();
+                    //if one of inputs are empty, send alert
+                    if (EmailValue.equals("") || PasswordValue.equals("") || RepasswordValue.equals("") || PhoneValue.equals("")) {
+                        Toast.makeText(getApplicationContext(), "Fields are empty, check your input.", Toast.LENGTH_SHORT).show();
+                    } else if(!check) {
+                        Toast.makeText(getApplicationContext(), "You must agree to term of service", Toast.LENGTH_SHORT).show();
+                    }else{
+                        //check the email is unique
+                        if (PasswordValue.equals(RepasswordValue)) {
+                            Boolean chkemail = db.email_Unique(EmailValue);
+                            if (chkemail) {//if email is unique, save data to database
+                                boolean insert = db.insert_user(EmailValue, PasswordValue, PhoneValue);
+                                if (insert) {//if successfully insert to database, message and jump to login
+                                    Toast.makeText(getApplicationContext(), "Registered Successfully!", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(Register.this, Login.class);
+                                    intent.putExtra("email", EmailValue);
+                                    startActivity(intent);
+                                }
+                            } else
+                                Toast.makeText(getApplicationContext(), "Email already exists", Toast.LENGTH_SHORT).show();
+                        } else
+                            Toast.makeText(getApplicationContext(), "Password doesn't match", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
-        });
+            });
 
-        Back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Register.this, Login.class);
-                startActivity(intent);
-            }
-        });
-    }
+            Back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Register.this, Login.class);
+                    startActivity(intent);
+                }
+            });
+        }
 }
 
 
